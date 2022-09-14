361d69f728a7eda4fc7a551b7cab93abd7ffbf5a
izacueio <izabella.coelhos@hotmail.com>
2022-09-14 19:23:46 -0400

    projeto JS
    Versionamento dos projetos JS.

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.5
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples, só faz algo se for verdadeiro!
+    console.log(`Vc ultrapassou a velocidade permitida, está multado.`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
---
+++ CursoJS/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'BRASIL'
+console.log(`Vivendo em ${país}`)
+if(país == 'BRASIL'){
+    console.log(`BRASILEIRO`)
+}else{
+    console.log(`Vc é estrangeiro.`)
+}
